2c733dfd153c848d5f27585398a0afe265be731b
Johannes Altmanninger <aclopte@gmail.com>
2021-01-17 18:01:11 +0100

    Remove tutorial from usage

--- _update-usage.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-open README_md, "README.md" and undef $/,
-scalar <README_md> =~ m/^## Usage\n(.*?)\n^##/ms, $Usage = $1,
-open Script, "+<", "git-branchless" and
-$New_Text = scalar <Script> =~ s/^(USAGE[^\n]*)\n.*?^("""\n)/$1$Usage$2/msgr,
-truncate Script, 0 and seek Script, 0, 0 and print Script $New_Text
-
-and system "black *.py"
